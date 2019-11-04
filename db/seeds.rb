fa63859110c92b65ed61f2f4a99d83a251da515d
Hope <hopegiometti@gmail.com>
2019-11-04 17:08:25 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+SchoolClass.destroy_all
+
+#students
+hermione = Student.create(first_name: "Hermione", last_name: "Granger")
+ron = Student.create(first_name: "Ron", last_name: "Weasley")
+harry = Student.create(first_name: "Harry", last_name: "Potter")
+
+#school classes 
+potions = SchoolClass.create(title: "Potions", room_number: -5)
+defense = SchoolClass.create(title: "Defense Against the Dark Arts", room_number: 15)
+transfiguration = SchoolClass.create(title: "Transfiguration", room_number: 2)
